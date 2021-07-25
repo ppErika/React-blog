a2f85e01c5f67eae8b22f5e6ad86d9441037f988
LAPTOP-KHDAKGR7\erika <hanerika11@gmail.com>
2021-07-26 01:34:14 +0900

    반복적으로 사용하는 친구는 컴포넌트를 이용하자 (하지만 state쓸 때 복잡해짐) div를 너무 많이 써서 보기 싫으면 프래그먼트(빈괄호) - <></> 를 이용해보자

--- src/App.js
+++ src/App.js
@@ -7,20 +7,19 @@ function App() {
   let [글제목, 글제목변경] = useState(['하이하이요', '우동 맛있겠다', '잠을 달라']);
   let [좋아요, 좋아요변경] = useState(0);
 
-  function 제목바꾸기(){
-    var newArray = [...글제목];
-    newArray[0]='바이바이요';
-    글제목변경(newArray);
-  }
+  // function 제목바꾸기(){
+  //   var newArray = [...글제목];
+  //   newArray[0]='바이바이요';
+  //   글제목변경(newArray);
+  // }
 
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <button onClick={ 제목바꾸기 }>버튼</button>
       <div className="list">
-        <h3> {글제목[0]} <span onClick={()=>{ return 좋아요변경(좋아요+1) }}>😘</span> {좋아요} </h3>
+        <h3> {글제목[0]} <span onClick={()=>{ 좋아요변경(좋아요+1) }}>😘</span> {좋아요} </h3>
         <p>7월 25일 발행</p>
         <hr/>
       </div>
@@ -34,8 +33,22 @@ function App() {
         <p>7월 26일 발행</p>
         <hr/>
       </div>
+
+      <Modal />
+      
     </div>
   );
 }
 
+function Modal(){
+  return(
+    <>
+    <div className="modal">
+        <h2>제목</h2>
+        <p>날짜</p>
+        <p>상세 내용</p>
+    </div>
+    </>
+  )
+}
 export default App;
