6cd67d36fcbb7a638a38ba894dc6a12615aa99d8
LAPTOP-KHDAKGR7\erika <hanerika11@gmail.com>
2021-07-26 00:04:44 +0900

    변수보다는 state(useState)를 사용해보자

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
-  let posts = '강남 고기 맛집';
+  let [글제목, 글제목변경] = useState(['하이하이요', '우동 맛있겠다']);
   
-  function ham() {
-    return 100
-  }
-
   return (
     <div className="App">
       <div className="black-nav">
-        <div style={{color:'yellow', fontSize: '30px'}}>개발 Blog</div>
+        <div>개발 Blog</div>
       </div>
-      <img src={logo} />
-      <h4>{posts}</h4>
-      <h2>{ham()}</h2>
+      <div className="list">
+        <h3> {글제목[0]} </h3>
+        <p>7월 25일 발행</p>
+        <hr/>
+      </div>
     </div>
   );
 }
