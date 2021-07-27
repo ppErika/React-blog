00b7baca4bcb20963670337981e2f37ad8386869
LAPTOP-KHDAKGR7\erika <hanerika11@gmail.com>
2021-07-27 12:10:41 +0900

    props를 이용해서 자식 컴포넌트에게 전달하기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,8 +6,7 @@ function App() {
 
   let [글제목, 글제목변경] = useState(['하이하이요', '우동 맛있겠다', '잠을 달라']);
   let [좋아요, 좋아요변경] = useState(0);
-
-  let[modal, modal변경] = useState(false);
+  let [modal, modal변경] = useState(false);
 
   function 제목정렬() {
     var newArray = [...글제목];
@@ -15,54 +14,44 @@ function App() {
     글제목변경(newArray);
   }
 
-  // function 반복된UI(){
-  //   var 어레이 =[];
-  //   for(var i=0; i<3; i++){
-  //     어레이.push(<div>안녕</div>);
-  //   }
-
-  //   return 어레이
-  // }
-
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <button onClick={ 제목정렬 }>제목 정렬하기</button>
-    
-      <button onClick={()=>{modal변경(!modal)}}>버튼</button>
-      {
-        modal===true
-        ? <Modal />
-        : null
-      }
-      
-      {/* { 반복된UI() } */}
+      <button onClick={제목정렬}>제목 정렬하기</button>
 
       {
-        글제목.map(function(글){
+        글제목.map(function (글) {
           return (
-          <div className="list">
-            <h3> {글} </h3>
-            <p>7월 25일 발행</p>
-            <hr/>
-          </div>
+            <div className="list">
+              <h3> {글} </h3>
+              <p>7월 25일 발행</p>
+              <hr />
+            </div>
           )
         })
       }
+
+      <button onClick={() => { modal변경(!modal) }}>열고 닫는 버튼</button>
+      {
+        modal === true
+          ? <Modal 자식글제목={글제목}></Modal>
+          : null
+      }
+
     </div>
   );
 }
 
-function Modal(){
-  return(
+function Modal(props) {
+  return (
     <>
-    <div className="modal">
-        <h2>제목</h2>
+      <div className="modal">
+        <h2>{props.자식글제목[2]}</h2>
         <p>날짜</p>
         <p>상세 내용</p>
-    </div>
+      </div>
     </>
   )
 }
