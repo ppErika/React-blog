cfd8adc6c1aa876cb61206d0d1179ba2123203ae
decidone <decidone@gmail.com>
2021-08-04 15:12:51 +0900

    백엔드 기능들 연동완료

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import listJson from './json/listJson.json';
 import writeJson from './json/writeJson.json';
+import axios from 'axios';
 
+const backendUrl = 'http://localhost:8080';
 
 function App() {
 
@@ -16,13 +18,24 @@ function App() {
 
   // 페이지가 열릴 때 json파일의 데이터를 받아와서 state에 저장
   useEffect(()=>{
-    var newArray = [...listJson];
-    setListData(newArray);
-    var newArray2 = [writeJson];
-    setWriteData(newArray2);
+
+    // json파일에서 데이터 가져오는 코드
+    // var newArray = [...listJson];
+    // setListData(newArray);
+    // var newArray2 = [writeJson];
+    // setWriteData(newArray2);
 
     // 백엔드 연동이 된다면 list를 요청하고
     // return을 받아서 list state에 넣는 방식으로 수정
+    axios.get(backendUrl + '/list')
+    .then((result)=>{
+      console.log(result.data)
+      var newArray = [...result.data];
+      setListData(newArray);
+    })
+    .catch(()=>{
+      console.log('failed-list')
+    })
   },[]);
 
   function 제목정렬() {
@@ -39,32 +52,75 @@ function App() {
     // arrayCopy2.unshift(0);
     // 좋아요변경(arrayCopy2);
 
-    // 글 추가를 위한 post데이터를 백엔드에 보내고
-    // return온 데이터를 listdata에 추가
     // 해당 테스트에선 return온 데이터를 writeData로 대체
-    var listDataCopy = [...listData];
-    var writedataCopy = [...writeData];
-    listDataCopy.unshift(writedataCopy[0]);
-    setListData(listDataCopy);
+    // var listDataCopy = [...listData];
+    // var writedataCopy = [...writeData];
+    // listDataCopy.unshift(writedataCopy[0]);
+    // setListData(listDataCopy);
+
+    // 글 추가를 위한 post데이터를 백엔드에 보내고
+    // return온 데이터를 listdata 맨 위에 추가
+    let form = new FormData();
+    form.append('context', 입력값);
+    axios.post(backendUrl + '/write', form)
+    .then((result)=>{
+      var listDataCopy = [...listData];
+      listDataCopy.unshift(result.data);
+      setListData(listDataCopy);
+    })
+    .catch(()=>{
+      console.log('failed-write');
+    })
   }
 
-  function 좋아요추가(i) {
-    var arrayCopy = [...좋아요];
-    arrayCopy[i]+=1;
-    좋아요변경(arrayCopy);
+  function 좋아요추가(id) {
+    // var arrayCopy = [...좋아요];
+    // arrayCopy[i]+=1;
+    // 좋아요변경(arrayCopy);
 
     // 글 id에 맞춰서 좋아요 추가 요청을 백엔드에 보내고
     // 성공하면 list스테이트에서 해당id의 좋아요 +=1
     // onClick 매서드의 좋아요추가(i)를 좋아요추가(data.id)로 사용하면 될 듯
+    axios.get(backendUrl + '/like/' + id)
+    .then((result)=>{
+      console.log(result.data)
+      var seq = getIndex(listData, 'id', id)
+      var listDataCopy = [...listData];
+      listDataCopy[seq].likes += 1;
+      setListData(listDataCopy);
+    })
+    .catch(()=>{
+      console.log('failed-like')
+    })
   }
 
   function 글삭제(id){
-    console.log(id);
+    //console.log(id);
+
+    axios.get(backendUrl + '/delete/' + id)
+    .then((result)=>{
+      //console.log(result.data)
+      var seq = getIndex(listData, 'id', id)
+      var listDataCopy = [...listData];
+      listDataCopy.splice(seq, 1);
+      setListData(listDataCopy);
+    })
+    .catch(()=>{
+      console.log('failed-delete')
+    })
 
     // 받아온 id로 백엔드에 delete요청
     // 문제없이 delete되면 list state에서 해당 id 글을 찾아서 삭제
   }
 
+  function getIndex(arr, prop, value) {
+    for(var i = 0; i < arr.length; i++) {
+      if(arr[i][prop] === value) {
+        return i;
+      }
+    }
+    return -1; //to handle the case where the value doesn't exist
+  }
   return (
     <div className="App">
       <div className="black-nav">
@@ -76,7 +132,7 @@ function App() {
         listData.map(function (data, i) {
           return (
             <div className="list" key={i}>
-              <h3 onClick={() => { 누른제목변경(i) }}> {data.context} <span onClick={ ()=>{ 좋아요추가(i) } }>😘</span> { data.likes }</h3>
+              <h3 onClick={() => { 누른제목변경(i) }}> {data.context} <span onClick={ ()=>{ 좋아요추가(data.id) } }>😘</span> { data.likes }</h3>
               <p>{data.issued}
                 <button onClick={() => { 글삭제(data.id) }} style={{ marginLeft: '5px' }}> 글 삭제 </button> 
               </p>
